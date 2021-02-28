01e29275bc37dc1419183c85c2b9a2543d96f700
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2021-02-28 01:21:25 +0100

    Change url on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl('blog/')}>
               Get Started
             </Link>
           </div>
