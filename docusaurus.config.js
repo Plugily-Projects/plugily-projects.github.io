b611c70f49ad68fefa9f6b3b89b245f5eb0badf3
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2021-03-09 20:25:33 +0100

    Changed link of feedback

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,7 +93,7 @@ module.exports = {
           items: [
             {
               label: 'Feedbacky',
-              href: 'feedback',
+              href: 'plugily/feedback',
             },
             {
               label: 'Discord',
