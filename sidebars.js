34a82e8651c2ce06f6e98746b580dee824db543d
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2023-06-25 22:24:13 +0200

    Updated sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -13,74 +13,58 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  // By default, Docusaurus generates a sidebar from the docs folder structure
-  minigames: [
-    {
-      type: 'category',
-      label: 'Minigames',
-      items: [
-        'minigame/overview',
+    // By default, Docusaurus generates a sidebar from the docs folder structure
+    minigames: [
         {
-        /*  Setup: [
-            {
-              type: 'link',
-              label: 'Tutorial video', // The label that should be displayed (string).
-              href: 'https://tutorial.plugily.xyz/' // The target URL (string).
-            },
-            'villagedefense/setup/simple',
-            'villagedefense/setup/cmdsandperms',
-            'villagedefense/setup/maps',
-          ],
-          Placeholders: [
-            'villagedefense/placeholders/placeholderapi',
-            'villagedefense/placeholders/scoreboard',
-          ],
-          Development: [
-            'villagedefense/development/api',
-            {
-              type: 'link',
-              label: 'JavaDocs', // The label that should be displayed (string).
-              href: 'https://jd.plugily.xyz/apidocs/minecraft/villagedefense/' // The target URL (string).
-            },
-            {
-              type: 'link',
-              label: 'Maven', // The label that should be displayed (string).
-              href: 'https://maven.plugily.xyz/plugily/projects/villagedefense/' // The target URL (string).
-            },
-          ],
-          Support: [
-            {
-              type: 'link',
-              label: 'Discord support', // The label that should be displayed (string).
-              href: 'https://discord.plugily.xyz/' // The target URL (string).
-            },
-            'villagedefense/support/files',
-            'villagedefense/support/faq.mdx',
-            'villagedefense/support/servertips',
-          ],
-          Addon: [
-            'villagedefense/addon/overview',
-            'villagedefense/addon/cmdsandperms',
-            'villagedefense/addon/files',
-            'villagedefense/addon/faq.mdx',
-          ],*/
+            type: 'category',
+            label: 'Minigames',
+            items: [
+                'minigame/overview',
+                'minigame/placeholders',
+                {
+                    Setup: [
+                        {
+                            type: 'link',
+                            label: 'Tutorial video', // The label that should be displayed (string).
+                            href: 'https://tutorial.plugily.xyz/' // The target URL (string).
+                        },
+                        'minigame/setup/simple-setup',
+                        'minigame/setup/cmdsandperms',
+                        'minigame/setup/maps',
+                    ],
+                    Development: [
+                        'minigame/development/api',
+                        {
+                            type: 'link',
+                            label: 'JavaDocs', // The label that should be displayed (string).
+                            href: 'https://maven.plugily.xyz/' // The target URL (string).
+                        },
+                        {
+                            type: 'link',
+                            label: 'Maven', // The label that should be displayed (string).
+                            href: 'https://maven.plugily.xyz/plugily/projects/' // The target URL (string).
+                        },
+                    ],
+                    Support: [
+                        {
+                            type: 'link',
+                            label: 'Discord support', // The label that should be displayed (string).
+                            href: 'https://discord.plugily.xyz/' // The target URL (string).
+                        },
+                        'minigame/support/files',
+                        'minigame/support/faq',
+                        'minigame/support/servertips',
+                    ],
+                    Addon: [
+                        'minigame/addon/overview',
+                        'minigame/addon/cmdsandperms',
+                        'minigame/addon/files',
+                        'minigame/addon/faq',
+                    ],
+                }
+            ]
         }
-      ]
-    }
-  ],
-
-  // But you can create a sidebar manually
-  /*
-  tutorialSidebar: [
-    'intro',
-    'hello',
-    {
-      type: 'category',
-      label: 'Tutorial',
-      items: ['tutorial-basics/create-a-document'],
-    },
-  ],
-   */
+    ],
 };
 
 module.exports = sidebars;
